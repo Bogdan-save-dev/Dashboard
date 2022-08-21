7b7a165808ca0dd519464a4d99f77609b1be52ce
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-21 11:34:07 +0300

    Enployees  component is completed

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Toolbar,
+  Search,
+  Inject,
+} from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
+
+import { employeesData, employeesGrid } from '../data/dummy'
 
 function Employees() {
-  return <div>Employees</div>
+  return (
+    <div
+      className="m-2 p-2 bg-white rounded-3xl
+    md:m-10  md:p-10"
+    >
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        dataSource={employeesData}
+        allowSorting
+        allowPaging
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, idx) => (
+            <ColumnDirective key={item + idx} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  )
 }
 
 export default Employees
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -23,12 +23,29 @@ function Orders() {
     md:m-10  md:p-10"
     >
       <Header category="Page" title="Orders" />
-      <GridComponent id="gridcomp" dataSource={ordersData}>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowSorting
+        allowPaging
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, idx) => (
             <ColumnDirective key={item + idx} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            ContextMenu,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   )
